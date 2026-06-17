78f1652a823c1403022cbdb444166e574c61e492
geunhh <rmsgnl111@gmail.com>
2026-06-18 05:13:50 +0900

    [YMC-149] chore: initialize backend repository

---
+++ src/main/java/com/ymc/api/YmcApiApplication.java
@@ -0,0 +1,13 @@
+package com.ymc.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YmcApiApplication.class, args);
+	}
+
+}
