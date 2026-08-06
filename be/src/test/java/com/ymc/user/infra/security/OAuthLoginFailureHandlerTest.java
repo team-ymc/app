3ee635b510155fae8bbb5923769de80c0aafdc63
geunhh <rmsgnl111@gmail.com>
2026-08-07 02:15:50 +0900

    [YMC-310] feat(be): 거부 사유를 not_allowed로 구분해 전달

--- be/src/test/java/com/ymc/user/infra/security/OAuthLoginFailureHandlerTest.java
+++ be/src/test/java/com/ymc/user/infra/security/OAuthLoginFailureHandlerTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
 
 import com.ymc.common.config.AuthProperties;
 
@@ -25,6 +27,39 @@ class OAuthLoginFailureHandlerTest {
 
         handler.onAuthenticationFailure(new MockHttpServletRequest(), response,
                 new AuthenticationException("취소됨") {});
+
+        assertThat(response.getRedirectedUrl())
+                .isEqualTo("http://localhost:5173/auth/popup-done.html?error=oauth_failed");
+    }
+
+    @Test
+    void 비허용_계정_거부는_not_allowed로_전달한다() throws Exception {
+        AuthProperties props = new AuthProperties(
+                "test-secret-key-that-is-32-bytes-long!!",
+                Duration.ofMinutes(30), Duration.ofDays(14), "http://localhost:5173", false,
+                Set.of());
+        OAuthLoginFailureHandler handler = new OAuthLoginFailureHandler(props);
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        handler.onAuthenticationFailure(new MockHttpServletRequest(), response,
+                new OAuth2AuthenticationException(new OAuth2Error("not_allowed", "거부", null)));
+
+        assertThat(response.getRedirectedUrl())
+                .isEqualTo("http://localhost:5173/auth/popup-done.html?error=not_allowed");
+    }
+
+    @Test
+    void 그_외_OAuth2_오류코드는_URL로_새지_않는다() throws Exception {
+        AuthProperties props = new AuthProperties(
+                "test-secret-key-that-is-32-bytes-long!!",
+                Duration.ofMinutes(30), Duration.ofDays(14), "http://localhost:5173", false,
+                Set.of());
+        OAuthLoginFailureHandler handler = new OAuthLoginFailureHandler(props);
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        handler.onAuthenticationFailure(new MockHttpServletRequest(), response,
+                new OAuth2AuthenticationException(
+                        new OAuth2Error("invalid_token_response", "내부 사정", null)));
 
         assertThat(response.getRedirectedUrl())
                 .isEqualTo("http://localhost:5173/auth/popup-done.html?error=oauth_failed");
